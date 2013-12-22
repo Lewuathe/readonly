16590a43195600bd75e25664e223db8057d49d7c
Lewuathe <lewuathe@me.com>
2013-12-22 23:18:26 +0900

    initial commit

---
+++ test/readonly-test.js
@@ -0,0 +1,106 @@
+var vows = require('vows'),
+    assert = require('assert');
+
+var readonly = require('../lib/index.js');
+var UnableRewriteException = require('../lib/UnableRewriteException.js');
+var _ = require('underscore');
+
+vows.describe('ReadOnly function').addBatch({
+    'Restrict rewrite': {
+        topic: function() {
+            var obj = {'A':'a', 'B':'b', 'C':'c'};
+            return obj;
+        },
+
+        'Writable from module A': function (topic) {
+            var moduleA = {};
+            moduleA.obj = topic;
+            moduleA.obj.A = 'd';
+            assert.equal(moduleA.obj.A, 'd');
+        },
+
+        'Not writable from module B': function (topic) {
+            var moduleB = {};
+            moduleB.obj = readonly(topic);
+            assert.throws(function() { moduleB.obj.B = 'b'; }, UnableRewriteException);
+//            assert.equal(moduleB.obj.B, 'b');
+        },
+    },
+
+    'Update and rewrite': {
+        topic: function() {
+            var obj = {'A':'a', 'B':'b', 'C':'c'};
+            return obj;
+        },
+        
+        'can rewrite from A and access from B': function (topic) {
+            var moduleA = {};
+            var moduleB = {};
+            moduleA.obj = topic;
+            moduleB.obj = readonly(topic);
+            moduleA.obj.A = 'd';
+
+            assert.equal(moduleB.obj.A, 'd');
+        }
+    },
+
+    'Reference of _original': {
+        topic: function() {
+            var obj = {'A':'a', 'B':'b', 'C':'c'};
+            return obj;
+        },
+        'cannot refer _original': function(topic) {
+            var moduleB = {};
+            moduleB.obj = readonly(topic);
+        }
+    },
+    
+    'Extends test': {
+        topic: function() {
+            var obj = {'A':'a', 'B':'b', 'C':'c'};
+            return obj;
+        },
+        
+        'can copy': function(obj) {
+            var obj2 = _.clone(obj);
+            obj2.A = 'd';
+            assert.equal(obj.A, 'a');
+        }
+    },
+
+    'Getter Test': {
+        topic: function() {
+            var obj = {'A':'a', 'B':'b', 'C':'c'};
+
+            function makeGetter(prop) {
+                return function() {
+                    var p = prop;
+                    return obj[p];
+                };
+            }
+
+            var _cloned = {};
+            for (var prop in obj) {
+                var getter = makeGetter(prop);
+                Object.defineProperty(_cloned, prop, {
+                    set: function() {
+                        throw new UnableRewriteException('original cannot be rewrite');
+                    },
+                    get: function() {
+                        return obj[prop]
+                    },
+                    enumerable: true
+                });
+                _cloned[prop];
+            }
+
+            return _cloned;
+        },
+
+        'get a': function(obj) {
+            assert.equal(obj.A, 'a');
+        }
+    }
+
+           
+}).export(module); // Export the Suite
